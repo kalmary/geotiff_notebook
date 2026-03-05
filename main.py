7c19cbfcf3cecf4f58a0467624f63b2652a19adf
kalmary <siniarskim@gmail.com>
2026-03-05 20:09:28 +0100

    genrator added

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ array = [1, 2, 3, 4, 5]
 def funkcja(array):
     big_list = []
     for i, element in enumerate(array):
-        new_list = [element for i in range(10000)]
+        new_list = [element for i in range(10000)] # FIXME: remove this
         
         yield new_list
 
@@ -13,5 +13,5 @@ list_gen = funkcja(array)
 
 for element in list_gen:
     a = element
-    print(a)
+    print(a) # TODO: Remove print statement
     
