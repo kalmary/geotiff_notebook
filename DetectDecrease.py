0243f1dbe62df0af5605ed17694dcc04f86441af
michal-siniarski <siniarskim@gmail.com>
2026-04-09 08:28:11 +0200

    detector improvements

--- DetectDecrease.py
+++ DetectDecrease.py
@@ -15,36 +15,62 @@ That's the only "parameter" and it has a direct statistical meaning.
 import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Literal
 from skimage.morphology import remove_small_objects, closing, disk
 import rasterio as rio
+from dataclasses import dataclass, field
 
 
-def detect_threshold(data: np.ndarray, k: float = -10.) -> dict:
-
-    valid = data[~np.isnan(data)]
-    threshold = float(valid.mean() - k * valid.std())
-
-    mask = (data < threshold) & ~np.isnan(data)
-    mask = closing(mask, disk(3))
-    mask = remove_small_objects(mask, max_size=20)
-
-    return mask
+class DetectorMethod:
+    method: Literal["threshold"] # + other methods later
+    cfg: dict
 
 
-if __name__ == "__main__":
-    # Example usage
-    path = Path("data/processed/czarna 121 2025-04-22-ORTHO-NDVI.data_augmented.tif")
-    with rio.open(path) as dataset:
-        ndvi = dataset.read(1)
-        nodata = dataset.nodata
-        ndvi = np.where(ndvi == nodata, np.nan, ndvi)
+class Detector:
+    def __init__(self, data: np.ndarray):
+        self._data = data
+        self._mask = None
 
-    damage_mask = detect_threshold(ndvi, 6.)
+    
 
-    plt.figure(figsize=(10, 6))
-    # plt.imshow(ndvi, cmap="RdYlGn", vmin=-1, vmax=1)
-    plt.imshow(damage_mask, cmap="Reds", alpha=0.5)
-    plt.title("Detected Damage (Red Overlay)")
-    plt.axis("off")
-    plt.show()
+    def _detect_threshold(self, cfg: dict) -> dict:
+
+        valid = self._data[~np.isnan(self._data)]
+        threshold = float(valid.mean() - float(cfg["k"]) * valid.std())
+
+        mask = (self._data < threshold) & ~np.isnan(self._data)
+        mask = closing(mask, disk(3))
+        mask = remove_small_objects(mask, max_size=20)
+
+        return mask
+
+    def _generate_mask(self, method: DetectorMethod) -> np.ndarray:
+
+        dispatch = {
+            "threshold": self._detect_threshold
+        }
+
+        return dispatch[method.method](method.cfg)
+
+    def apply(self, method: DetectorMethod) -> "Detector":
+        self._mask = self._generate_mask(method)
+
+    @property
+    def get_mask(self):
+        if self._mask is not None:
+            return self._mask
+        else:
+            raise ValueError("No mask could be found. Most likely no detection method was applied.")
+        
+    @property
+    def get_original_data(self):
+        return self._data
+    
+    @property
+    def get_masked_data(self):
+        if self._mask is not None:
+            return self._data[self._mask]
+        else:
+            raise ValueError("No mask could be found. Most likely no detection method was applied.")
+
+
